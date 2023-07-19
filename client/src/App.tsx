24b943c38093073eac6849b9ef5ece7519ad51d3
bernborgess <bernborgess@outlook.com>
2023-07-19 02:23:39 -0300

    working add

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,51 @@
 import { useEffect, useState } from 'react';
 import { api } from './api';
 
+interface Task {
+  id: number;
+  title: string;
+  complete: boolean;
+};
+
 function App() {
-  const [message, setMessage] = useState<string | null>(null);
+  const [tasks, setTasks] = useState<Task[] | null>(null);
 
-  async function fetchMessage() {
+  async function fetchTasks() {
     try {
-      const { data } = await api.get("/members");
-      setMessage(data.msg as string);
-      console.log(data)
+      const { data } = await api.get("");
+      setTasks(data);
     } catch (err) {
-
+      console.error(err);
     }
   }
 
-  useEffect(() => { fetchMessage() }, []);
+  useEffect(() => { fetchTasks() }, []);
+
+  async function addTask() {
+    try {
+      await api.post("/add")
+    }
+    catch (err) {
+      console.error(err);
+    }
+  }
+
 
   return (
     <div>
-      {message && <p>{message}</p>}
-      <h1>Vite + React</h1>
+      <h1>Todo List!</h1>
+      {
+        !tasks
+          ? <h3>loading</h3> :
+          <div>
+            {tasks.map((task) => (
+              <h3 key={task.id}>{task.title}</h3>
+            ))}
+          </div>
+      }
+      <button onClick={addTask}>
+        MORE
+      </button>
     </div>
   )
 }
--- server/server.py
+++ server/server.py
@@ -1,14 +1,68 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_restful import Api, Resource, fields, marshal_with
 from flask_cors import CORS
 
 app = Flask(__name__)
+
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+api = Api(app)
+app.app_context().push()
 CORS(app, supports_credentials=True)
 
 
-@app.route("/members")
-def members():
-    data = {"msg": "Hello"}
-    return jsonify(data)
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    complete = db.Column(db.Boolean)
+
+
+todo_fields = {"id": fields.Integer, "title": fields.String, "complete": fields.Boolean}
+
+
+class TodoResource(Resource):
+    @marshal_with(todo_fields)
+    def get(self):
+        return Todo.query.all()
+
+
+api.add_resource(TodoResource, "/")
+
+
+@app.get("/")
+def home():
+    todo_list = Todo.query.all()
+    return jsonify(todo_list)
+
+
+@app.post("/add")
+def add():
+    new_todo = Todo(title="TITLE", complete=False)
+    db.session.add(new_todo)
+    db.session.commit()
+    return jsonify({"msg": "OK"})
+
+
+@app.get("/update/<int:todo_id>")
+def update(todo_id):
+    todo = db.session.query(Todo).filter(Todo.id == todo_id).first()
+    if not todo:
+        return jsonify({"msg": "FAIL"})
+
+    todo.complete = not todo.complete
+    db.session.commit()
+    return jsonify({"msg": "OK"})
+
+
+@app.get("/delete/<int:todo_id>")
+def delete(todo_id):
+    todo = db.session.query(Todo).filter(Todo.id == todo_id).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return jsonify({"msg": "OK"})
 
 
 if __name__ == "__main__":
